c9dbdcc5a433be54043ae507e86b285130b13592
Boris Kim <366793@gmail.com>
2022-08-15 23:23:21 +0900

    debuging

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(requestLogger);
 app.use(cors({
-  origin: 'https://your-movie-explorer-fr.nomoredomains.work',
+  origin: ['https://your-movie-explorer-fr.nomoredomains.work', 'https://api.nomoreparties.co/beatfilm-movies'],
   credentials: true,
 }));
 
